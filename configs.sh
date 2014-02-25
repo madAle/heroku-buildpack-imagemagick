8b00d27a5b6c58b75f933dea3e8aeb7576b614d0
Alessandro Verlato <averlato@gmail.com>
2014-02-25 20:03:14 +0100

    maybe...

--- configs.sh
+++ configs.sh
@@ -1,7 +1,7 @@
 
 export LIBPNG_VERSION="1.6.9"
 export LIBWEBP_VERSION="0.3.1"
-export IMAGE_MAGICK_VERSION="6.8.8-7"
+export IMAGE_MAGICK_VERSION="0.1.2"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
 export LIBWEBP_REMOTE_PATH="libwebp-${LIBWEBP_VERSION}.tgz"
