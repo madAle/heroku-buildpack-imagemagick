e933b5a30e2c5a5fe34c0ad85f21be6ea5eaf97f
Alessandro Verlato <averlato@gmail.com>
2014-02-25 14:53:15 +0100

    Added libwebp

--- configs.sh
+++ configs.sh
@@ -1,6 +1,9 @@
 
 export LIBPNG_VERSION="1.6.9"
+export LIBWEBP_VERSION="0.3.1"
 export IMAGE_MAGICK_VERSION="6.8.8-7"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
+export LIBWEBP_REMOTE_PATH="libwebp-${LIBWEBP_VERSION}.tgz"
 export S3_BUCKET="my-heroku-buildpack-imagemagick"
+export VULCAN_HEROKU_APP_NAME="fancy-builderver"
